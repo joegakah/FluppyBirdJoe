8b5df61c8a2ada4b352ee67e748090f6c6b41974
JosephGakah <joegakah@gmail.com>
2023-08-02 15:56:38 +0300

    Adjusted moving speed,  spawn rate, sizes

--- Assets/BarrierScript.cs
+++ Assets/BarrierScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BarrierScript : MonoBehaviour
 {
-    public float moveSpeed = 5;
+    public float moveSpeed = 10;
     public float deadZone = -45;
     // Start is called before the first frame update
     void Start()
