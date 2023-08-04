b9c4b71d2767eef1373eaca86062175982a605a7
JosephGakah <joegakah@gmail.com>
2023-08-04 22:03:55 +0300

    Game over Screen

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
@@ -13,6 +14,11 @@ public class LogicScript : MonoBehaviour
     {
         playerScore += addToScore;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
